59d0487891a5072409e089a03c7edef1b6b345f5
Mirella Souza <devmirella@gmail.com>
2024-12-11 02:15:17 -0300

    feat: header mobile

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,22 +1,24 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import { useState } from "react";
 import Banner from "../../components/Banner/Banner"
 import Header from "../../components/Header/Header"
 import ProductList from "../../components/ProductList/ProductList";
 import Newsletter from "../../components/Newsletter/Newsletter";
 import Footer from "../../components/Footer/Footer";
+import HeaderMobile from "../../components/HeaderMobile/HeaderMobile";
 
 
 function Home() {
-
+    const [isMobile] = useState(window.innerWidth <= 920);
     return (
         <>
-         <Header/>
-         <Banner/>
-         <ProductList />
-         <Newsletter />
-         <Footer />
+            {isMobile ? <HeaderMobile/> : <Header/>}
+            <Banner/>
+            <ProductList />
+            <Newsletter />
+            <Footer />
         </>
     )
   }
